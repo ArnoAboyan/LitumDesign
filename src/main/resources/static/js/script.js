40944fad82675cea360f2658754cc80939fda1bb
withkeygames <withkeygames@gmail.com>
2023-08-28 23:41:08 +0300

    v0.02 index page prepare

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,16 @@
+
+  
+  //search by click button
+  function handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      console.log("obj")
+      return true; 
+    } else {
+      return false; 
+    }
+  }
+
+
+
+
+
