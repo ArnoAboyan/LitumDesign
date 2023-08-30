277cdc128850efe1e1b0478971be2cfc3f8dd03f
withkeygames <withkeygames@gmail.com>
2023-08-30 22:35:27 +0300

    v0.03 submitafile bussines logic

---
+++ src/main/java/com/litumdesign/LitumDesign/Entity/TestClass.java
@@ -0,0 +1,9 @@
+package com.litumdesign.LitumDesign.Entity;
+
+import lombok.Data;
+
+@Data
+public class TestClass {
+    public String username;
+    public String password;
+}
--- src/main/java/com/litumdesign/LitumDesign/controller/FileController.java
+++ src/main/java/com/litumdesign/LitumDesign/controller/FileController.java
@@ -1,26 +1,55 @@
 package com.litumdesign.LitumDesign.controller;
 
+import com.litumdesign.LitumDesign.Entity.ProductEntity;
+import com.litumdesign.LitumDesign.Entity.TestClass;
+import com.litumdesign.LitumDesign.auth.AppUserController;
+import com.litumdesign.LitumDesign.formaticUI.Toast;
+import com.litumdesign.LitumDesign.repository.ProductEntityRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/file")
+@RequiredArgsConstructor
 public class FileController {
 
-    @GetMapping("/submitafile")
-    public String submitFilePage (){
+   private final ProductEntityRepository productEntityRepository;
+
+    @GetMapping("/addfile")
+    public String submitFilePage(@ModelAttribute TestClass testClass) {
         return "submitFilePage";
 
     }
 
 
-    @GetMapping("/addfile")
-    public String addFile(Model model){
 
 
-        return "redirect:/";
+    @PostMapping("/addfile")
+    public String addFile(@Valid TestClass testClass, Errors errors, Model model, RedirectAttributes attributes) {
+
+        if (errors.hasErrors()) {
+            attributes.addFlashAttribute("toast", Toast.error("File not add!", "all bad"));
+            // Если есть ошибки валидации, вернуть представление с ошибками
+            System.out.println("---------> 1");
+            return "submitfilepage"; // Имя вашего представления
+        }
+
+        try {
+
+            attributes.addFlashAttribute("toast", Toast.success("Файл успешно добавлен", "all good"));
+            System.out.println("---------> 2");
+            return "redirect:/file/addfile";
+        } catch (Exception e) {
+            attributes.addFlashAttribute("toast", Toast.error("File not add!", e.getMessage()));
+            System.out.println("---------> 3");
+            return "redirect:/file/addfile";
+        }
+
     }
 
 }
