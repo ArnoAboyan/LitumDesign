8ea9b7ab8d73c63723aed7bfdba88b941aeda6eb
withkeygames <withkeygames@gmail.com>
2023-09-26 15:30:13 +0300

    v0.17 bugfix

--- src/main/java/com/litumdesign/LitumDesign/controller/NewsController.java
+++ src/main/java/com/litumdesign/LitumDesign/controller/NewsController.java
@@ -87,11 +87,11 @@ public class NewsController {
             model.addAttribute("link", "/news/getallnewshx?page=" + (news.getNumber() + 1));
 //        model.addAttribute("link", "/getallnewshx?page=" + news.nextOrLastPageable());
 
-            return "newsfragment";
+            return "/fragments/newsfragment";
         }
         log.info("END");
         model.addAttribute("end", "end");
-        return "newsfragment";
+        return "/fragments/newsfragment";
     }
 
 
