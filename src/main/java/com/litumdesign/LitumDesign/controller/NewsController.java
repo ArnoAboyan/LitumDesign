ede09181403d5b627a9ed6d925713f2e30057bd8
withkeygames <withkeygames@gmail.com>
2023-09-26 16:06:38 +0300

    v0.17 bugfix

--- src/main/java/com/litumdesign/LitumDesign/controller/NewsController.java
+++ src/main/java/com/litumdesign/LitumDesign/controller/NewsController.java
@@ -102,7 +102,7 @@ public class NewsController {
 
         model.addAttribute("particularnews", newsEntityService.getParticularNews(newsId));
 
-                return "/fragments/particularnewsfragment";
+                return "fragments/particularnewsfragment";
     }
 }
 
