eca5675db0d5251485fe6cda42da8bd314429aea
withkeygames <withkeygames@gmail.com>
2023-10-02 16:03:51 +0300

    v0.24  bugfix

--- src/main/java/com/litumdesign/LitumDesign/controller/FileController.java
+++ src/main/java/com/litumdesign/LitumDesign/controller/FileController.java
@@ -76,7 +76,8 @@ public class FileController {
             System.out.println("WE HAVE SOME PROBLEMS " + e.getMessage());
         }
 
-        return "index";
+
+        return "redirect:/";
     }
 
 
--- src/main/java/com/litumdesign/LitumDesign/controller/HomeController.java
+++ src/main/java/com/litumdesign/LitumDesign/controller/HomeController.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.SQLOutput;
@@ -23,6 +24,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/")
 public class HomeController {
     private final ProductEntityService productEntityService;
 
