873e2fc0ddbaf5f99cf3d1bafb6323e50c31ce1e
withkeygames <withkeygames@gmail.com>
2023-10-13 00:06:08 +0300

    v0.29 search panel

--- src/main/java/com/litumdesign/LitumDesign/repository/ProductEntityRepository.java
+++ src/main/java/com/litumdesign/LitumDesign/repository/ProductEntityRepository.java
@@ -21,7 +21,7 @@ public interface ProductEntityRepository extends JpaRepository<ProductEntity, Lo
     ProductEntity findByGdFileId(String fieldId);
 
     @Query("SELECT p from ProductEntity p where " +
-    "concat(p.title, p.description, p.shortInfo) " +
+    "concat(p.title, p.shortInfo) " +
     " LIKE %:searchQuery%")
     Page<ProductEntity> searchByInput(@Param("searchQuery") String searchQuery, Pageable pageable);
 }
