4728b2df438423f2fa244cda82672e299eaf1c56
withkeygames <withkeygames@gmail.com>
2023-10-27 00:33:47 +0300

    v0.56 bugfix

--- src/test/java/com/litumdesign/LitumDesign/LitumDesignApplicationTests.java
+++ src/test/java/com/litumdesign/LitumDesign/LitumDesignApplicationTests.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EnableConfigurationProperties(NationConfigProperties.class)
 class LitumDesignApplicationTests {
 
 
